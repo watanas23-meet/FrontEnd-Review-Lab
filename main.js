839e474bfecc7f83dd791495204eef2ad3e7f288
watanas23-meet <pintelzyt@gmail.com>
2022-08-01 14:06:26 +0300

    bonus-added

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-function changeBackgroundColor(color) {
-    document.body.style.backgroundColor= color;
+
+
+const element = document.getElementById("myBtn");
+element.addEventListener("click", myFunction);
+var counter = 0;
+
+function myFunction() {
+
+	if (counter%2==0){
+		document.body.style.backgroundColor = "yellow";
+	}
+	else{
+		document.body.style.backgroundColor = "blue";
+	}
+	
+	counter += 1;
+
 }
